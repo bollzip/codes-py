824a5a07e3f404870da134abf868dd14b031b73b
Bollzip <pedrinho34pedro@gmail.com>
2024-09-23 17:56:58 -0300

    Commmit

---
+++ aula py 2.py
@@ -0,0 +1,40 @@
+# print ("Digite seu nome")
+# nome = input()
+# print (nome)
+# idade = input ("digite sua idade: ")
+# print (type(idade))
+# salario = int (input('Salario?: '))
+# imposto = float (input("Digite o imposto: "))
+# sal= float (input("digite salario: "))
+# print (sal)
+# z = 1755/11
+# print ("%.2f" % z)
+# idade1 = 10
+# idade2 = 12
+# filho = 2
+# print ("Eu tenho", filho, "filhos, ambos de" , idade1, "e" , idade2, "anos")
+# a = "Pedro"
+# b = "Bollzip"
+# print ("Prezado "  +a+ " " +b+ "." + "Olá")
+# print (10*"Bollzip\n")
+# print ((" "*10+ "/"+ "\ \n"), end="")
+# print ((" "*9+ "/"+ " "*2+ "\ \n"), end="")
+# print ((" "*8+ "/"+ " "*4+ "\ \n"), end="")
+# print ((" "*7+ "/"+ " "*6+ "\ \n"), end="")
+# print ((" "*6+ "/"+ " "*8+ "\ \n"), end="")
+# print (" "*5+ "/" +5* "_" "_"+"\ ")
+# frase = 'um tringualo de base igual a {0} e altura igual {1} possui área igual a {2}'. format(3, 4, 12)
+# print (frase)
+# lingaguem = "Python"
+# print  (f"Programando em {lingaguem}")
+# x= (2/3)
+# print (f"printando o {x}")
+# print (f"printando o {x : .2f}")
+
+# f = float (input ("Fº: "))
+# c = (5/9) * (f - 32)
+# print (f"Celius:  {c : .2f}")
+
+
+
+
---
+++ test1.py
@@ -0,0 +1,33 @@
+# print ("mamao com banana")
+# print (2+3)
+# print ("\\")
+# print ('dia', 'mês', 'ano', sep='/')
+# print ('3.144')
+# print (type(3.14))
+
+# print (type(42000))
+# print (type (42.000))
+# print (type (42))
+# print ("10" + "10")
+
+# print (type ('esse é um string'))
+# print (type ("E esse tambem é um string"))
+# print (type ("""E esse tambem"""))
+# print (type ('''e mesmo esse'''))
+# 
+# mensagem = "não contavam com minha astucia"
+# n = 2021
+#pi = 3.14159
+# print(type(mensagem))
+# print (type(n))
+ #print (type (pi)
+# print (81 ** 0.5)
+# peso = 80
+# altura = 1.85
+# imc = peso * altura
+# print (imc)
+
+nome = input ("Digite seu nome")
+print (nome)
+
+
